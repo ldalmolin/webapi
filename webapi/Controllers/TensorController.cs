ab589199160dbdf7fd20c335023fa95f5ee1f759
Luca Dal Molin <luca.dalmolin@soprasteria.com>
2018-02-13 11:57:03 +0100

    tensorflow webapi test

---
+++ webapi/Controllers/TensorController.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using TensorFlow;
+
+namespace webapi.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    public class TensorController : Controller
+    {
+
+        public TensorController(ILogger<TensorController> logger)
+        {
+            _logger = logger;
+        }
+
+        private readonly ILogger _logger;
+        [HttpGet]
+        public object Calculate(int p1, int p2)
+        {
+            using (var session = new TFSession())
+            {
+                var graph = session.Graph;
+
+                var a = graph.Const(p1);
+                var b = graph.Const(p2);
+                _logger.LogInformation("a=2 b=3");
+
+                // Add two constants
+                var addingResults = session.GetRunner().Run(graph.Add(a, b));
+                _logger.LogInformation("a+b={0}", addingResults.GetValue());
+
+                // Multiply two constants
+                var multiplyResults = session.GetRunner().Run(graph.Mul(a, b));
+                _logger.LogInformation("a*b={0}", multiplyResults.GetValue());
+                return Ok(addingResults.GetValue());
+            }
+
+            
+        }
+
+    }
+}
--- webapi/Startup.cs
+++ webapi/Startup.cs
@@ -67,12 +67,15 @@ namespace webapi
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, WebapiContext context)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, WebapiContext context, ILoggerFactory loggerFactory)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            //log
+            loggerFactory.AddDebug();
 
             // ===== Use Authentication ======
             app.UseAuthentication();
